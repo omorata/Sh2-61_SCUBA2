4cb86c9e0cd3067819ad153a89a94b9ad0887fa6
Oscar Morata <omoratax@gmail.com>
2020-03-11 11:45:16 +0100

    Makefile_reduction.mk: clean Makefile of all unused stuff

--- config/Makefile_reduction.mk
+++ config/Makefile_reduction.mk
@@ -2,15 +2,12 @@
 ##  Makefile to run scripts to reduce the JCMT SCUBA2 data of Sh2-61
 ##
 ##  O. Morata
-##  2018-19
+##  2018-20
 ##
 
 ##-- Info --------------------------------------------------------------
 HOME_DIR=/lustre/opsw/work/omoratac/Sh2-61/SCUBA2
 SNAME=Sh2_61
-
-#dayreductions = 850r0
-#days = 0424 0425
 
 jointreductions = j850r0 j850r1 j450r0 j450r1
 jointreductions += j850r0_mb j850r1_mb j450r0_mb j450r1_mb
@@ -32,89 +29,6 @@ export
 
 
 ##-- Template definition -----------------------------------------------
-
-# Template to define the rules to process steps of each different day for
-# the given reduction
-#
-define Days_Template
-
-.PHONY: reduce-$(1)-$(2) cal-$(1)-$(2) checkcal-$(1)-$(2)
-
-
-reduce-$(1)-$(2) : $(RES_DIR)/$(1)/$(SNAME)-$(1)-$(2)-reduc.sdf
-
-cal-$(1)-$(2): $(RES_DIR)/$(1)/$(SNAME)-$(1)-$(2)-reduc_cal.sdf
-
-
-$(RES_DIR)/$(1)/$(SNAME)-$(1)-$(2)-reduc.sdf: $(CFG_DIR)/reduc-$(1)-$(2).cfg
-	. $(BIN)/reduce_raw.sh $(CFG_DIR)/reduc-$(1)-$(2).cfg
-
-
-$(RES_DIR)/$(1)/$(SNAME)-$(1)-$(2)-reduc_cal.sdf: $(RES_DIR)/$(1)/$(SNAME)-$(1)-$(2)-reduc.sdf
-	$(BIN)/post_scuba2.sh  \
-		-a cal  \
-		-d $(RES_DIR)/$(1)/ \
-		-p $(CFG_DIR)/recipe-$(1)-$(2).cfg  \
-		-i $(SNAME)-$(1)-$(2)-reduc
-
-
-checkcal-$(1)-$(2):
-	. $(BIN)/check_fcf.sh \
-		-d $(RES_DIR)/$(1)  \
-		-c $(CFG_DIR)/reduc-$(1)-$(2).cfg
-
-endef
-
-
-
-# Template to run the rest of the steps of the defined reduction
-#
-define Tag_Template
-
-.PHONY: reduce-$(1)-days cal-$(1)-days checkcal-$(1)
-.PHONY: coadd-$(1) snr-$(1) crop-$(1)
-
-
-reduce-$(1)-days: $(reduce_$(1)_list)
-
-cal-$(1)-days: $(cal_$(1)_list)
-
-checkcal-$(1): $(checkcal_$(1)_list)
-
-coadd-$(1): $(RES_DIR)/$(1)/$(SNAME)-$(1)-coadd.sdf
-
-snr-$(1): $(RES_DIR)/$(1)/$(SNAME)-$(1)-coadd_snr.sdf
-
-crop-$(1): $(RES_DIR)/$(1)/$(SNAME)-$(1)-coadd_crop.sdf
-
-$(eval mosaic_dir := $(CFG_DIR)/mosaic_lists)
-
-
-$(RES_DIR)/$(1)/$(SNAME)-$(1)-coadd.sdf:  $(jfiles_$(1)_list) $(mosaic_dir)/$(1)-mosaic.list
-	$(BIN)/post_scuba2.sh  \
-		-a add  \
-		-d $(RES_DIR)/$(1) \
-		-p $(CFG_DIR)/recipe-$(1).cfg \
-		-l $(mosaic_dir)/$(1)-mosaic.list  \
-		-o $(SNAME)-$(1)-coadd
-
-
-$(RES_DIR)/$(1)/$(SNAME)-$(1)-coadd_snr.sdf: $(RES_DIR)/$(1)/$(SNAME)-$(1)-coadd.sdf
-	$(BIN)/post_scuba2.sh \
-		-a snr  \
-		-d $(RES_DIR)/$(1)  \
-		-i $(SNAME)-$(1)-coadd
-
-
-$(RES_DIR)/$(1)/$(SNAME)-$(1)-coadd_crop.sdf: $(RES_DIR)/$(1)/$(SNAME)-$(1)-coadd.sdf
-	$(BIN)/post_scuba2.sh  \
-		-a crop  \
-		-d $(RES_DIR)/$(1) \
-		-p $(CFG_DIR)/recipe-$(1).cfg  \
-		-i $(SNAME)-$(1)-coadd
-
-endef
-
 
 
 # Template to process all days of the same reduction on the same step
@@ -170,85 +84,11 @@ endef
 
 ##-- End of template definition ----------------------------------------
 
-##-- Generate lists ----------------------------------------------------
-
-# loop to generate the lists needed for the rules that join data from more
-# than one day
-#
-$(foreach reduction, $(dayreductions),\
-    $(eval reduce_$(reduction)_list = ) \
-    $(eval cal_$(reduction)_list = ) \
-    $(eval checkcal_$(reduction)_list = ) \
-    $(eval jfiles_$(reduction)_list = ) \
-    $(foreach day, $(days),\
-        $(eval reduce_$(reduction)_list += reduce-$(reduction)-$(day)) \
-        $(eval cal_$(reduction)_list += cal-$(reduction)-$(day)) \
-        $(eval checkcal_$(reduction)_list += checkcal-$(reduction)-$(day)) \
-        $(eval jfiles_$(reduction)_list += $(RES_DIR)/$(reduction)/$(SNAME)-$(reduction)-$(day)-reduc_cal.sdf) \
-))
-
-##-- Enf of lists ------------------------------------------------------
-
 
 # define rules for joint reductions
 #
 $(foreach reduction, $(jointreductions),\
     $(eval $(call Joint_Template,$(reduction)))\
 )
-
-
-#850r0: reduce-850r0-days cal-850r0-days coadd-850r0 snr-850r0 crop-850r0
-#850r1: reduce-850r1-days cal-850r1-days coadd-850r1 snr-850r1 crop-850r1
-
-#450r0: reduce-450r0-days cal-450r0-days coadd-450r0 snr-450r0 crop-450r0
-#450r1: reduce-450r1-days cal-450r1-days coadd-450r1 snr-450r1 crop-450r1
-
-#850r1mf: reduce-850r1mf-days cal-850r1mf-days coadd-850r1mf snr-850r1mf crop-850r1mf mf-850r1mf-days
-
-
-# define rules for day reductions
-#
-$(foreach reduction, $(dayreductions),\
-    $(foreach day, $(days), \
-        $(eval $(call Days_Template,$(reduction),$(day)))\
-))
-
-$(foreach reduction, $(dayreductions),\
-    $(eval $(call Tag_Template,$(reduction)))\
-)
-
-.PHONY: ratios
-.PHONY: ratio-j450r0_mb__j850r0_mb ratio-j450r0_mb__j850r1_mb
-
-
-ratios: ratio-j450r0_mb__j850r0_mb ratio-j450r0_mb__j850r1_mb ratio-j450r0_mb__j850r0_co_mb
-
-$(eval rj450r0mfj850r0mf := $(RES_DIR)/ratios/ratio-j450r0_mb__j850r0_mb.sdf)
-
-ratio-j450r0_mb__j850r0_mb: $(rj450r0mfj850r0mf)
-
-$(rj450r0mfj850r0mf): $(CFG_DIR)/ratio-j450r0_mb__j850r0_mb.cfg $(RES_DIR)/j450r0_mb/$(SNAME)-j450r0_mb-reduc.sdf $(RES_DIR)/j850r0_mb/$(SNAME)-j850r0_mb-reduc.sdf
-	sh $(BIN)/mkratios.sh \
-		-c $(CFG_DIR)/ratio-j450r0_mb__j850r0_mb.cfg \
-		-d $(RES_DIR)/ratios
-
-
-$(eval rj450r0mfj850r1mf := $(RES_DIR)/ratios/ratio-j450r0_mb__j850r1_mb.sdf)
-
-ratio-j450r0_mb__j850r1_mb: $(rj450r0mfj850r1mf)
-
-$(rj450r0mfj850r1mf): $(CFG_DIR)/ratio-j450r0_mb__j850r1_mb.cfg $(RES_DIR)/j450r0_mb/$(SNAME)-j450r0_mb-reduc.sdf $(RES_DIR)/j850r1_mb/$(SNAME)-j850r1_mb-reduc.sdf
-	sh $(BIN)/mkratios.sh \
-		-c $(CFG_DIR)/ratio-j450r0_mb__j850r1_mb.cfg \
-		-d $(RES_DIR)/ratios
-
-$(eval rj450r0mfj850r0contmf := $(RES_DIR)/ratios/ratio-j450r0_mb__j850r0_co_mb.sdf)
-
-ratio-j450r0_mb__j850r0_co_mb: $(rj450r0mfj850r0contmf)
-
-$(rj450r0mfj850r0contmf): $(CFG_DIR)/ratio-j450r0_mb__j850r0_co_mb.cfg $(RES_DIR)/j450r0_mb/$(SNAME)-j450r0_mb-reduc.sdf $(RES_DIR)/j850r0_co_mb/$(SNAME)-j850r0_co_mb-reduc.sdf
-	sh $(BIN)/mkratios.sh \
-		-c $(CFG_DIR)/ratio-j450r0_mb__j850r0_co_mb.cfg \
-		-d $(RES_DIR)/ratios
 
 
