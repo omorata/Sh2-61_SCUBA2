bdc90bd935ce4c8488ae3ccbbfbf5649b7c90604
Oscar Morata <omoratax@gmail.com>
2020-09-19 21:48:02 +0200

    [Makefile_analysis] Beginning of changes to add Histo and XYPlots

--- config/Makefile_analysis.mk
+++ config/Makefile_analysis.mk
@@ -657,6 +657,41 @@ endef
 
 
 
+define HistoPlots
+# Template to plot histograms from the pixel maps of calculated physical
+# parameters
+#
+# Arguments: 1- tgt, 2- findclump id, 3- physcalc tag, 4-histoplot tag
+#
+# The plots depend on calcs.log as a generic way  and on the wildcard of
+# a .yml file with the same stem as the pdf
+#
+# The output name must have histo between arg 3 and arg 4
+#
+# models
+#  Sh2_61-j850r0_co_mb__j450ro_mb-fw_01_ref-histo-N_all.pdf
+#
+endef
+
+
+
+define XYPlots
+# Template to plot xyplots from the pixel maps of calculated physical
+# parameters
+#
+# Arguments: 1- tgt, 2- findclump id, 3- physcalc tag, 4-xyplot tag
+#
+# the plots depend on calcs.log as a generic way  and on the wildcard of
+# a .yml file with the same stem as the pdf
+#
+# The output name must have xyplot between arg 3 and arg 4
+#
+# models
+#  Sh2_61-j850r0_co_mb__j450ro_mb-fw_01_ref-xyplot-N_vs_tdust_all.pdf
+#
+endef
+
+
 ##-- End of template definition ----------------------------------------
 
 
@@ -684,6 +719,10 @@ $(foreach tgt, $(targets),\
 
 # define rules for combined and findclumps_id
 #
+# changes to CalcPhysParam to add physcalc_tags as a third parameter and
+# as a fourth parameter to MapPhysParam
+#
+#  add call to HistoPlots and XYPlots with a fourth parameter
 $(foreach tgt, $(combined),\
     $(eval $(call Align_Dataset,$(tgt)))\
     $(foreach fc, $(fcs),\
