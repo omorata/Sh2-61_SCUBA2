16ca78a2e0ef6a6ee5fbb4a29b2bb786362ac784
Oscar Morata <omoratax@gmail.com>
2020-09-23 10:53:13 +0200

    split prepdata.mk from Makefile_analysis.mk

--- config/Makefile_analysis.mk
+++ config/Makefile_analysis.mk
@@ -56,172 +56,28 @@ export
 #
 include $(CFG_DIR)/reduction.mk
 
-#include $(CFG_DIR)/????.mk
-#include $(CFG_DIR)/findclumps.mk
 
-
-##-- Template definition -----------------------------------------------
-
-define PlotMaps
-# Template to plot maps for targets
+# functions to prepare daata for further processing and to plot the maps
+# of the reduced data
 #
-# Parameter: 1- target
+include $(CFG_DIR)/prepdata.mk
+
+
+# include function to include rules to look for clumps
 #
-$(eval map_dir := $(RES_DIR)/analysis_maps)
-
-$(eval reduc_file := $(RES_DIR)/$(1)/$(SNAME)-$(1)-reduc.sdf)
-$(eval reduc_snrfile := $(RES_DIR)/$(1)/$(SNAME)-$(1)-reduc_snr.sdf)
-
-$(eval fits_reducfile := $(map_dir)/$(SNAME)-$(1)-reduc.fits)
-$(eval fits_reducsnrfile := $(map_dir)/$(SNAME)-$(1)-reduc_snr.fits)
-
-$(fits_reducfile): $(reduc_file)
-	@ $(BIN_DIR)/prepare_maps.sh \
-                 -f $(reduc_file) \
-                 -o $(fits_reducfile) \
-                 -t "tofits" 
-
-$(fits_reducsnrfile): $(reduc_snrfile)
-	@ $(BIN_DIR)/prepare_maps.sh \
-                 -f $(reduc_snrfile) \
-                 -o $(fits_reducsnrfile) \
-                 -t "tofits"
-
-tofits-$(1): $(fits_reducfile) $(fits_reducsnrfile)
-.PHONY: tofits-$(1)
-
-tofits: tofits-$(1)
-.PHONY: tofits
-
-clean-fits_datasets-$(1):
-	@rm -fv $(fits_origfile)
-	@rm -fv $(fits_origsnrfile)
-.PHONY: clean-fits_datasets-$(1)
-
-clean-fits_datasets: clean-fits_datasets-$(1)
-.PHONY: clean-fits_datasets
-
-
-
-$(eval orig_file := $(RES_DIR)/analysis_maps/$(SNAME)-$(1).fits)
-
-$(eval map_file := $(map_dir)/$(SNAME)-$(1)-map.pdf)
-$(eval cfg_file := $(CFG_DIR)/figures/$(SNAME)-$(1)-map.yml)
-
-$(map_file): $(orig_file) $$(wildcard $$(cfg_file))
-	@if [ -f $(cfg_file) ]; then \
-	     $(EXT_DIR)/dbxmap.py \
-                 -c $(cfg_file) \
-                 -o $(map_dir) \
-                 -w $(map_dir) ;\
-         else \
-             echo -e "\n++ Ignoring rule:\n      $(out_fc)" ;\
-             echo -e "    No cfg file:\n      $(cfg_file)" ;\
-         fi
-
-plotmap-$(1): $(map_file)
-.PHONY: plotmap-$(1)
-
-plotmaps: plotmap-$(1)
-.PHONY: plotmaps
-
-clean-plotmap-$(1):
-	@rm -fv $(map_file)
-.PHONY: clean-plotmap-$(1)
-
-clean-plotmaps: clean-plotmap-$(1)
-.PHONY: clean-plotmaps
-
-endef
-
-
-
-
-define PrepareDataset
-# Template to prepare data files for further processing
-#
-#  Parameter: 1- target
-#
-$(eval out_dir := $(RES_DIR)/analysis_maps)
-
-$(eval tgt_dir := $(RES_DIR)/$(1))
-
-$(eval orig_file := $(tgt_dir)/$(SNAME)-$(1)-reduc.sdf)
-$(eval orig_snrfile := $(tgt_dir)/$(SNAME)-$(1)-reduc_snr.sdf)
-
-$(eval strip_file := $(out_dir)/$(SNAME)-$(1).sdf)
-$(eval strip_snrfile := $(out_dir)/$(SNAME)-$(1)-snr.sdf)
-
-$(strip_file): $(orig_file) 
-	@ $(BIN_DIR)/prepare_maps.sh \
-                 -f $(orig_file) \
-                 -o $(strip_file) \
-                 -t "strip" 
-
-$(strip_snrfile): $(orig_snrfile) 
-	@$(BIN_DIR)/prepare_maps.sh \
-                 -f $(orig_snrfile) \
-                 -o $(strip_snrfile) \
-                 -t "strip" 
-
-.PHONY: strip-$(1)
-strip-$(1): $(strip_file) $(strip_snrfile)
-
-.PHONY: strip
-strip: strip-$(1)
-
-
-
-
-$(eval fits_stripfile := $(out_dir)/$(SNAME)-$(1).fits)
-$(eval fits_stripsnrfile := $(out_dir)/$(SNAME)-$(1)-snr.fits)
-
-$(fits_stripfile): $(strip_file)
-	@ $(BIN_DIR)/prepare_maps.sh \
-            -f $(strip_file) \
-            -o $(fits_stripfile) \
-            -t "tofits" 
-
-$(fits_stripsnrfile): $(strip_snrfile)
-	@ $(BIN_DIR)/prepare_maps.sh \
-            -f $(strip_snrfile) \
-            -o $(fits_stripsnrfile) \
-            -t "tofits" 
-
-.PHONY: tofits_strip-$(1)
-tofits_strip-$(1): $(fits_stripfile) $(fits_stripsnrfile)
-
-.PHONY: tofits_strip
-tofits_strip: tofits_strip-$(1)
-
-
-.PHONY: clean-strip-$(1)
-clean-strip-$(1):
-	@rm -fv $(strip_file)
-	@rm -fv $(strip_snrfile)
-	@rm -fv $(fits_stripfile)
-	@rm -fv $(fits_stripsnrfile)
-
-.PHONY: clean-strip
-clean-strip: clean-strip-$(1)
-
-
-
-endef
-
-
-
 include $(CFG_DIR)/findclumps.mk
 
 
+# include function to calculate the physical parameters of the data
+#
 include $(CFG_DIR)/physcalcs.mk
 
 
+# function to include the rules that plot maps and plots of the
+# calculated physical parameters
+#
 include $(CFG_DIR)/physplots.mk
 
-
-
-##-- End of template definition ----------------------------------------
 
 
 list_tasks := reduction
